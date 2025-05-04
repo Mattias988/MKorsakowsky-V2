0174a899c00c17718c1e2ec282939c6eec19b2a3
Maciej <maciek.korsakowski@gmail.com>
2025-05-04 22:29:34 +0200

    add abut section

---
+++ frontend/src/sections/About/About.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import LaptopImg from '@/assets/laptop-image.png'
+import Button from "@/components/Button/button";
+import styles from './About.module.scss'
+
+export default function About() {
+    return (
+        <section className={styles.heroSection}>
+            <motion.img
+                src={LaptopImg.src}
+                alt="My Laptop"
+                initial={{ y: 300 }}
+                animate={{ y: 0 }}
+                exit={{ y: -1000 }}
+                transition={{ duration: 1 }}
+            />
+            <div className={styles.textContent}>
+                <motion.p
+                    initial={{ y: 300 }}
+                    animate={{ y: 0 }}
+                    exit={{ y: -1000 }}
+                    transition={{ duration: 0.6 }}
+                >
+                    About me
+                </motion.p>
+                <motion.h1
+                    initial={{ y: 300 }}
+                    animate={{ y: 0 }}
+                    exit={{ y: -1000 }}
+                    transition={{ duration: 0.8 }}
+                >
+                    Who am I?
+                </motion.h1>
+                <motion.p
+                    initial={{ y: 300 }}
+                    animate={{ y: 0 }}
+                    exit={{ y: -1000 }}
+                    transition={{ duration: 1 }}
+
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis venenatis  lectus non faucibus elementum. Quisque elementum, augue sit amet  imperdiet pharetra, risus diam tristique risus, sit amet pharetra lorem  massa vitae felis. Etiam malesuada, nisi vel pharetra bibendum, magna  magna laoreet erat, eu posuere purus dui sed sapien.
+                </motion.p>
+                <Button />
+            </div>
+        </section>
+    );
+}
