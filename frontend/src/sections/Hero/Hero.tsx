cd74892a538f739fb6319005c0fe8dbc6104311a
Mattias988 <maciek.korsakowski@gmail.com>
2025-05-05 13:14:47 +0200

    add styling and typewriter

--- frontend/src/sections/Hero/Hero.tsx
+++ frontend/src/sections/Hero/Hero.tsx
@@ -2,18 +2,31 @@
 
 import { motion } from 'framer-motion';
 import styles from './Hero.module.scss'
+import Typewriter from 'typewriter-effect';
 
 export default function Hero() {
     return (
         <section className={styles.heroSection}>
-                <motion.h1
-                    initial={{ y: 300 }}
-                    animate={{ y: 0 }}
-                    exit={{ y: -1000 }}
-                    transition={{ duration: 0.8 }}
-                >
-                    I am Maciek. I&#39;m Frontend Developer
-                </motion.h1>
+            <motion.h1
+                initial={{ y: 300 }}
+                animate={{ y: 0 }}
+                exit={{ y: -1000 }}
+                transition={{ duration: 0.8 }}
+            >
+                Hello, I’m Maciek.
+            </motion.h1>
+            <Typewriter
+                options={{
+                    loop: true,
+                }}
+                onInit={(typewriter) => {
+                    typewriter
+                        .typeString("I'm Frontend Developer")
+                        .pauseFor(2500)
+                        .deleteAll()
+                        .start();
+                }}
+            />
         </section>
     );
 }
