e94ece4dbcbef33709e0089aad2b426cb6132998
Mattias988 <maciek.korsakowski@gmail.com>
2025-05-08 15:13:16 +0200

    change tag to button and duration of animation

--- frontend/src/components/Button/button.tsx
+++ frontend/src/components/Button/button.tsx
@@ -3,14 +3,14 @@ import styles from "./Button.module.scss"
 
 export default function Button(){
     return (
-        <motion.div
+        <motion.button
             className={styles.btn}
             initial={{ y: 1300 }}
             animate={{ y: 0 }}
             exit={{ y: -1000 }}
-            transition={{ duration: 1, delay: 0.5 }}
+            transition={{ duration: 0.8, delay: 0.24 }}
         >
             See more!
-        </motion.div>
+        </motion.button>
     )
 }
