fea7453a1d775156c88f630ed7c95801f0a80eb3
Mattias988 <maciek.korsakowski@gmail.com>
2025-05-09 12:42:22 +0200

    change animation of about section

--- frontend/src/sections/About/About.tsx
+++ frontend/src/sections/About/About.tsx
@@ -6,38 +6,39 @@ import Button from "@/components/Button/button";
 import styles from './About.module.scss'
 
 export default function About() {
+
     return (
         <section className={styles.aboutSection}>
             <motion.img
                 src={LaptopImg.src}
                 alt="My Laptop"
-                initial={{ y: 1300 }}
-                animate={{ y: 0 }}
-                exit={{ y: -1000 }}
-                transition={{ duration: 0.8, delay: 0.21 }}
+                initial={{ x: -1300 }}
+                animate={{ x: 0 }}
+                exit={{ x: -1300}}
+                transition={{ duration: 1, ease: 'easeOut', delay: 0.1 }}
             />
             <div className={styles.textContent}>
                 <motion.p
-                    initial={{ y: 1300 }}
-                    animate={{ y: 0 }}
-                    exit={{ y: -1000 }}
-                    transition={{ duration: 0.8, delay: 0.22 }}
+                    initial={{ x: 1300 }}
+                    animate={{ x: 0 }}
+                    exit={{ x: 1300}}
+                    transition={{ duration: 1, ease: 'easeOut', delay: 0.4 }}
                 >
                     About me
                 </motion.p>
                 <motion.h1
-                    initial={{ y: 1300 }}
-                    animate={{ y: 0 }}
-                    exit={{ y: -1000 }}
-                    transition={{ duration:0.8,delay: 0.23 }}
+                    initial={{ x: 1300 }}
+                    animate={{ x: 0 }}
+                    exit={{ x: 1300}}
+                    transition={{ duration:1, ease: 'easeOut', delay: 0.6 }}
                 >
                     Who am I?
                 </motion.h1>
                 <motion.p
-                    initial={{ y: 1300 }}
-                    animate={{ y: 0 }}
-                    exit={{ y: -1000 }}
-                    transition={{ duration: 0.8, delay: 0.24 }}
+                    initial={{ x: 1300 }}
+                    animate={{ x: 0 }}
+                    exit={{ x: 1300}}
+                    transition={{ duration: 1, ease: 'easeOut', delay: 0.7 }}
                 >
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis venenatis  lectus non faucibus elementum. Quisque elementum, augue sit amet  imperdiet pharetra, risus diam tristique risus, sit amet pharetra lorem  massa vitae felis. Etiam malesuada, nisi vel pharetra bibendum, magna  magna laoreet erat, eu posuere purus dui sed sapien.
                 </motion.p>
