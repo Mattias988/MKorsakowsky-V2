1ed284d2b13916b6bacff8d8036fcf89a5a40036
Mattias988 <maciek.korsakowski@gmail.com>
2025-05-09 12:43:42 +0200

    change animation of timeline items

--- frontend/src/components/Timeline/TimelineItem/TimelineItem.tsx
+++ frontend/src/components/Timeline/TimelineItem/TimelineItem.tsx
@@ -1,29 +1,85 @@
 'use client';
 
 import styles from '../TimelineItem/TimelineItem.module.scss';
+import { motion } from 'framer-motion';
 
 interface TimelineItemProps {
+    index: number;
     title?: string;
     date: string;
     description: string;
     onSeeMore?: () => void;
 }
 
-export default function TimelineItem({ date, title, description, onSeeMore }: TimelineItemProps) {
+export default function TimelineItem({ date, title, description, onSeeMore, index }: TimelineItemProps) {
+
+    const itemVariants = {
+        hidden: { x: '-100vw', opacity: 0 },
+        visible: (index: number) => ({
+            x: 0,
+            opacity: 1,
+            transition: {
+                delay: 1 + index * 0.3,
+                duration: 1,
+                ease: 'easeOut',
+            },
+        }),
+        exit: {
+            x: '-100vw',
+            opacity: 0,
+            transition: { duration: 2 },
+        },
+    };
 
     return (
-        <div className={styles.timelineItem}>
-            <div className={styles.timelineItemDate}>{date}</div>
-            <div className={styles.timelineItemMarkerContainer}>
+        <motion.div
+            className={styles.timelineItem}
+            variants={itemVariants}
+            custom={index}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+        >
+            <motion.div
+                className={styles.timelineItemDate}
+                initial={{ x: 1300 }}
+                animate={{ x: 0 }}
+                exit={{ x: -1000 }}
+                transition={{ duration: 1, delay: 0.6 }}
+            >
+                {date}
+            </motion.div>
+            <motion.div
+                className={styles.timelineItemMarkerContainer}
+                initial={{ x: 1300 }}
+                animate={{ x: 0 }}
+                exit={{ x: -1000 }}
+                transition={{ duration: 1, delay: 0.7 }}
+            >
                 <div className={styles.timelineItemMarker}></div>
-            </div>
+            </motion.div>
             <div className={styles.timelineItemContainer}>
                 {title && <h3 className={styles.timelineItemTitle}>{title}</h3>}
-                <p className={styles.timelineItemDescription}>{description}</p>
+                <motion.p
+                    className={styles.timelineItemDescription}
+                    initial={{ x: 1300 }}
+                    animate={{ x: 0 }}
+                    exit={{ x: -1000 }}
+                    transition={{ duration: 1, delay: 0.8 }}
+                >{description}</motion.p>
                 { onSeeMore && (
-                    <button className={styles.timelineItemButton} onClick={onSeeMore}>See more!</button>
+                    <motion.button
+                        className={styles.timelineItemButton}
+                        onClick={onSeeMore}
+                        initial={{ x: 1300 }}
+                        animate={{ x: 0 }}
+                        exit={{ x: -1000 }}
+                        transition={{ duration: 1, delay: 0.9 }}
+                    >
+                        See more!
+                    </motion.button>
                 )}
             </div>
-        </div>
+        </motion.div>
     )
 };
