01b623e5a20993df79963121cedcdd051c209a41
Mattias988 <maciek.korsakowski@gmail.com>
2025-12-07 21:37:32 +0100

    feat: add TitleAndDesc component with styles for title and description layout

---
+++ frontend/src/components/TitleAndDesc/TitleAndDesc.tsx
@@ -0,0 +1,20 @@
+import {TitleAndDescProps} from "@/types/components";
+import styles from "@/sections/About/About.module.scss";
+
+export default function TitleAndDesc({title , description}: TitleAndDescProps) {
+
+    return (
+        <div className={styles.titleWithDescContainer}>
+            <div className={styles.titleContainer}>
+                <div className={styles.firstTitleLine}/>
+                    <h2 className={styles.sectionTitle}>{title}</h2>
+                <div className={styles.secondTitleLine}/>
+            </div>
+            <div className={styles.descContainer}>
+                <p className={styles.sectionDesc}>
+                    {description}
+                </p>
+            </div>
+        </div>
+    )
+}
