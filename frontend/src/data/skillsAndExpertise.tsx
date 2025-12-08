5227801b12e06c629990657c5fdd7ca57f847858
Mattias988 <maciek.korsakowski@gmail.com>
2025-12-08 22:52:21 +0100

    feat: restructure Skills and Expertise section with detailed skill cards and icons

--- frontend/src/data/skillsAndExpertise.tsx
+++ frontend/src/data/skillsAndExpertise.tsx
@@ -1,18 +1,63 @@
 import {SkillsAndExpertiseTitleAndDesc} from "@/types";
+import {SkillsAndExpertise} from "@/types/components";
+import {PanelsTopLeft, Server, Database, Smartphone} from "lucide-react";
 
-export const skillsAndExpertise: SkillsAndExpertiseTitleAndDesc = {
+export const skillsAndExpertiseTitleAndDesc: SkillsAndExpertiseTitleAndDesc = {
     title: "Skills & Expertise",
     description: "A diverse skill set to tackle any challenge",
-    skills: [
-        "JavaScript/TypeScript",
-        "React.js & Next.js",
-        "Node.js & Express",
-        "HTML5 & CSS3",
-        "RESTful APIs & GraphQL",
-        "Database Management (SQL & NoSQL)",
-        "Version Control (Git & GitHub)",
-        "Responsive Web Design",
-        "UI/UX Principles",
-        "Agile & Scrum Methodologies"
-    ]
 }
+
+export const skillsAndExpertise: SkillsAndExpertise = [
+    {
+        cardTitle: "Frontend Development",
+        icon: PanelsTopLeft,
+        skills: [
+            "React",
+            "TypeScript",
+            "JavaScript",
+            "HTML5",
+            "CSS3",
+            "Sass",
+            "Tailwind CSS",
+            "Responsive Design"
+        ],
+    },
+    {
+        cardTitle: "Backend Development",
+        icon: Server,
+        skills: [
+            "Node.js",
+            "Express.js",
+            "Python",
+            "Django",
+            "RESTful APIs",
+            "GraphQL",
+            "Database Management (SQL & NoSQL)"
+        ],
+    },
+    {
+        cardTitle: "Database & Tools",
+        icon: Database,
+        skills: [
+            "MySQL",
+            "PostgreSQL",
+            "MongoDB",
+            "Git & GitHub",
+            "Docker",
+            "CI/CD Pipelines",
+            "Cloud Services (AWS, Azure)"
+        ],
+    },
+    {
+        cardTitle: "Mobile & Other",
+        icon: Smartphone,
+        skills: [
+            "React Native",
+            "Flutter",
+            "UI/UX Principles",
+            "Agile Methodologies",
+            "Problem-Solving",
+            "Team Collaboration"
+        ]
+    }
+]
