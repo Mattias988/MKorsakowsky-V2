5140b8665a46139212686ec439da4f89480664a9
Mattias988 <maciek.korsakowski@gmail.com>
2025-12-08 22:51:57 +0100

    feat: update SkillsAndExpertiseCards with improved layout and structure for skill display

--- frontend/src/components/SkillsAndExpertiseCards/SkillsAndExpertiseCards.tsx
+++ frontend/src/components/SkillsAndExpertiseCards/SkillsAndExpertiseCards.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import styles from './SkillsAndExpertiseCards.module.scss';
-import { SkillsAndExpertise} from "@/types/components";
+import {SkillCard} from "@/types/components";
 
-export default function SkillsAndExpertiseCards({title, icon: Icon, skills}: SkillsAndExpertise) {
+export default function SkillsAndExpertiseCards({cardTitle, icon: Icon, skills}: SkillCard) {
 
     return (
-        <div className={styles.skillCardContainer}>
-            <div className={styles.iconContainer}>
-                <Icon className={styles.icon} />
-            </div>
-            <div className={styles.titleContainer}>
-                <h4>{title}</h4>
+        <div className={styles.skillCardsContainer}>
+            <div className={styles.skillCardContainer}>
+                <div className={styles.iconContainer}>
+                    <Icon className={styles.icon} />
+                </div>
+                <div className={styles.titleContainer}>
+                    <h4>{cardTitle}</h4>
+                </div>
             </div>
             <div className={styles.skillsContainer}>
-                <p>{skills}</p>
+                {skills.map((skill, index)=> (
+                    <div className={styles.skillContainer} key={index}>
+                         <p>{skill}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )
