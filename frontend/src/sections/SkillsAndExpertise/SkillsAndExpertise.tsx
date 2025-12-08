135d06e424fb8738741d2f3a0870db7171fa293f
Mattias988 <maciek.korsakowski@gmail.com>
2025-12-08 22:51:52 +0100

    feat: enhance SkillsAndExpertise section with responsive card layout and title/description integration

--- frontend/src/sections/SkillsAndExpertise/SkillsAndExpertise.tsx
+++ frontend/src/sections/SkillsAndExpertise/SkillsAndExpertise.tsx
@@ -1,16 +1,20 @@
 import styles from './SkillsAndExpertise.module.scss';
 import TitleAndDesc from "@/components/TitleAndDesc/TitleAndDesc";
-import {skillsAndExpertise} from "@/data/skillsAndExpertise";
+import {skillsAndExpertise, skillsAndExpertiseTitleAndDesc} from "@/data/skillsAndExpertise";
+import SkillsAndExpertiseCards from "@/components/SkillsAndExpertiseCards/SkillsAndExpertiseCards";
 
 
 export default function SkillsAndExpertise() {
-    const { title, description } = skillsAndExpertise;
-
+    const { title, description } = skillsAndExpertiseTitleAndDesc;
     return (
         <section id="skills-and-expertise" className={styles.skillsSection}>
             <TitleAndDesc title={title} description={description}/>
 
-
+            <div className={styles.skillCardsContainer}>
+                {skillsAndExpertise.map((item, index) => (
+                    <SkillsAndExpertiseCards key={index} cardTitle={item.cardTitle} icon={item.icon} skills={item.skills}/>
+                ))}
+            </div>
         </section>
     );
 }
