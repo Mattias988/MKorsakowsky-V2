3cf96f8c760a99768d86f13ab5abecfe605c3f64
Mattias988 <maciek.korsakowski@gmail.com>
2025-12-13 22:35:08 +0100

    feat: enhance Experience and Timeline components with new layout, animations, and title/description integration

---
+++ frontend/src/data/experience.tsx
@@ -0,0 +1,7 @@
+import {TitleAndDescProps} from "@/types/components";
+
+
+export const experienceTitleAndDesc:TitleAndDescProps = {
+    title: "Experience",
+    description: "A journey through my professional milestones and achievements."
+}
--- frontend/src/sections/Experience/Experience.tsx
+++ frontend/src/sections/Experience/Experience.tsx
@@ -1,27 +1,19 @@
 'use client';
 
 import React from 'react';
-import { motion } from 'framer-motion';
-import styles from './Experience.module.scss'; // Tu trzymaj style tylko dla nagłówka sekcji
+import styles from './Experience.module.scss';
 import { experienceData } from '@/data/timeLineData';
 import Timeline from "@/components/Timeline/Timeline/Timeline";
+import TitleAndDesc from "@/components/TitleAndDesc/TitleAndDesc";
+import {experienceTitleAndDesc} from "@/data/experience";
 
 export default function Experience() {
+    const { title, description } = experienceTitleAndDesc;
     return (
         <section className={styles.experienceSection} id="experience">
             <div className={styles.container}>
-                <motion.div
-                    className={styles.header}
-                    initial={{ opacity: 0, y: 20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.5 }}
-                    viewport={{ once: true }}
-                >
-                    <h2>Professional Experience</h2>
-                    <p>Moja ścieżka rozwoju i kluczowe projekty.</p>
-                </motion.div>
+                <TitleAndDesc title={title} description={description} />
 
-                {/* Przekazujemy dane bezpośrednio do komponentu */}
                 <Timeline data={experienceData} />
             </div>
         </section>
