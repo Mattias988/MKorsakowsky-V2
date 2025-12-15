b09db00dcac61081ad3f99a00553e0a81df72d4a
Mattias988 <maciek.korsakowski@gmail.com>
2025-12-15 20:12:50 +0100

    feat: implement Featured Projects section with project cards and updated title/description structure

--- frontend/src/types/components.ts
+++ frontend/src/types/components.ts
@@ -1,4 +1,5 @@
 import {LucideIcon} from "lucide-react";
+import { StaticImageData } from "next/image";
 
 export interface Card {
     title: string;
@@ -16,5 +17,14 @@ export interface SkillCard {
     icon: LucideIcon,
     skills: string[]
 }
+
+export interface ProjectCardProps {
+    title: string;
+    description: string;
+    technologies: string[];
+    src: StaticImageData | string;
+}
+
+export type ProjectsCards = ProjectCardProps[];
 
 export type SkillsAndExpertise = SkillCard[];
