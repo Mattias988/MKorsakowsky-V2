49bf05b79691de2135e3bff07f2adc45f6c4b327
Mattias988 <maciek.korsakowski@gmail.com>
2025-12-21 17:58:59 +0100

    feat: refactor Contact section layout and styles for improved responsiveness

--- frontend/src/sections/Contact/Contact.tsx
+++ frontend/src/sections/Contact/Contact.tsx
@@ -12,16 +12,26 @@ export default function Contact() {
         <section className={styles.contactContainer} id="contact">
             <TitleAndDesc title={title} description={description}/>
 
-            <main className={styles.contactMain}>
-                <div className={styles.contactItemsContainer}>
-                    {contactItemsData.map((contactItemsData, index) => (
-                        <ContactItem
-                            key={index}
-                            contactItemTitle={contactItemsData.contactItemTitle}
-                            contactItemDescription={contactItemsData.contactItemDescription}
-                            icon={contactItemsData.icon}
-                        />
-                    ))}
+            <main className={styles.mainInfoContainer}>
+
+                <div className={styles.contactLeftContainer}>
+                    <div className={styles.contentLeft}>
+                        <h2 className={styles.mainInfoTitle}>Get in Touch</h2>
+                        <p className={styles.mainInfoDescription}>
+                            I'm open to discussing new projects, creative ideas, or opportunities to be part of your visions.
+                        </p>
+                    </div>
+
+                    <div className={styles.contactItems}>
+                        {contactItemsData.map((contactItemsData, index) => (
+                            <ContactItem
+                                key={index}
+                                contactItemTitle={contactItemsData.contactItemTitle}
+                                contactItemDescription={contactItemsData.contactItemDescription}
+                                icon={contactItemsData.icon}
+                            />
+                        ))}
+                    </div>
                 </div>
 
                 <div className={styles.contactFormContainer}>
