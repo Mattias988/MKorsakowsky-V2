848ec1722d0b9517c5fa70fca5264bdd327a7d9b
Mattias988 <maciek.korsakowski@gmail.com>
2025-12-21 17:59:05 +0100

    feat: enhance ContactForm styles and structure for better usability and aesthetics

--- frontend/src/components/ContactForm/ContactForm.tsx
+++ frontend/src/components/ContactForm/ContactForm.tsx
@@ -1,13 +1,43 @@
 import styles from './ContactForm.module.scss'
+import {Send} from "lucide-react";
 
 export default function ContactForm() {
     return (
         <div className={styles.contactForm}>
             <form className={styles.form}>
-                <input type="text" name="name" placeholder="Your Name" required className={styles.input} />
-                <input type="email" name="email" placeholder="Your Email" required className={styles.input} />
-                <textarea name="message" placeholder="Your Message" required className={styles.textarea}></textarea>
-                <button type="submit" className={styles.button}>Send Message</button>
+
+                <div className={styles.formGroup}>
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Your name..."
+                        required
+                        className={styles.input}
+                    />
+                </div>
+
+                <div className={styles.formGroup}>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="example@email.com"
+                        required
+                        className={styles.input}
+                    />
+                </div>
+
+                <div className={styles.formGroup}>
+                    <label htmlFor="message">Message</label>
+                    <textarea name="message" placeholder="Write whatever you want..." required className={styles.textarea}></textarea>
+                </div>
+
+                <button type="submit" className={styles.button}>
+                    <Send width={18}/>
+                    Send Message
+                </button>
+
             </form>
         </div>
     )
