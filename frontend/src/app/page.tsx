fd2ebc2c71a9d3f7e28e186734706c0d96c17063
Mattias988 <maciek.korsakowski@gmail.com>
2026-02-05 21:15:22 +0100

    feat: implement foundational frontend components, sections, styling, and types for the application.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import Hero from '@/sections/Hero/Hero';
 import About from "@/sections/About/About";
-import { ThemeProvider } from '@/components/ThemeProvider/ThemeProvider';
 import Navbar from '@/components/Navbar/Navbar';
 import styles from '@/app/page.module.scss'
 import SkillsAndExpertise from "@/sections/SkillsAndExpertise/SkillsAndExpertise";
@@ -10,21 +9,31 @@ import Experience from "@/sections/Experience/Experience";
 import FeaturedProjects from "@/sections/FeaturedProjects/FeaturedProjects";
 import Contact from '@/sections/Contact/Contact';
 import Footer from '@/sections/Footer/Footer';
+import { Reveal } from '@/components/Reveal/Reveal';
 
 export default function Page() {
-
   return (
-      <ThemeProvider>
-        <div id="home" className={styles.pageView}>
-          <Navbar />
-          <Hero />
+    <>
+      <Navbar />
+      <div id="home" className={styles.pageView}>
+        <Hero />
+        <Reveal>
           <About />
+        </Reveal>
+        <Reveal>
           <SkillsAndExpertise />
+        </Reveal>
+        <Reveal>
           <Experience />
+        </Reveal>
+        <Reveal>
           <FeaturedProjects />
+        </Reveal>
+        <Reveal>
           <Contact />
-          <Footer />
-        </div>
-      </ThemeProvider>
+        </Reveal>
+        <Footer />
+      </div>
+    </>
   );
 }
