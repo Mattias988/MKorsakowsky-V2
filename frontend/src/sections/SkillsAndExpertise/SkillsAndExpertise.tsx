c70bde6c4195d31249cd56d18cd682d4e69a47a9
Mattias988 <maciek.korsakowski@gmail.com>
2026-02-06 19:37:45 +0100

    feat: implement the SkillsAndExpertise section component and add IDE configuration files.

--- frontend/src/sections/SkillsAndExpertise/SkillsAndExpertise.tsx
+++ frontend/src/sections/SkillsAndExpertise/SkillsAndExpertise.tsx
@@ -1,6 +1,6 @@
 import styles from './SkillsAndExpertise.module.scss';
 import TitleAndDesc from "@/components/TitleAndDesc/TitleAndDesc";
-import {skillsAndExpertise, skillsAndExpertiseTitleAndDesc} from "@/data/skillsAndExpertise";
+import { skillsAndExpertise, skillsAndExpertiseTitleAndDesc } from "@/data/skillsAndExpertise";
 import SkillsAndExpertiseCards from "@/components/SkillsAndExpertiseCards/SkillsAndExpertiseCards";
 
 
@@ -8,11 +8,11 @@ export default function SkillsAndExpertise() {
     const { title, description } = skillsAndExpertiseTitleAndDesc;
     return (
         <section id="skills-and-expertise" className={styles.skillsSection}>
-            <TitleAndDesc title={title} description={description}/>
+            <TitleAndDesc title={title} description={description} />
 
             <div className={styles.skillCardsContainer}>
                 {skillsAndExpertise.map((item, index) => (
-                    <SkillsAndExpertiseCards key={index} cardTitle={item.cardTitle} icon={item.icon} skills={item.skills}/>
+                    <SkillsAndExpertiseCards key={index} cardTitle={item.cardTitle} icon={item.icon} skills={item.skills} />
                 ))}
             </div>
         </section>
